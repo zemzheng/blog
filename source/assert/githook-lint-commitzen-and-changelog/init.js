20c2f8b0b1ea968dd484c35480d48142c7b12b36
zemzheng <zemzheng@gmail.com>
2019-08-02 15:51:28 +0800

    fix(lint): 本项目的 lint 没有安装成功

--- source/assert/githook-lint-commitzen-and-changelog/init.js
+++ source/assert/githook-lint-commitzen-and-changelog/init.js
@@ -1,55 +1,39 @@
-const fs = require('fs');
-const pkgjson = require('./package.json');
+const fs = require("fs");
+const pkgjson = require("./package.json");
 
-
-const [ useNpx, useEslint, useTslint ] = process.argv[ 2 ].split(/\s/g).map(
-    one => {
-        const val = one.toLowerCase();
-        return !!(val && val !== 'n');
-    }
-);
+const [useNpx, useEslint, useTslint] = process.argv[2].split(/\s/g).map(one => {
+  const val = one.toLowerCase();
+  return !!(val && val !== "n");
+});
 
 pkgjson.husky = {
-    hooks: {
-        "pre-commit": "lint-staged",
-        "commit-msg": useNpx
-            ? "npx commitlint -E HUSKY_GIT_PARAMS"
-            : "commitlint -E HUSKY_GIT_PARAMS"
-    },
-}
+  hooks: {
+    "pre-commit": "lint-staged",
+    "commit-msg": useNpx
+      ? "npx commitlint -E HUSKY_GIT_PARAMS"
+      : "commitlint -E HUSKY_GIT_PARAMS"
+  }
+};
 pkgjson.config = {
-    commitizen: {
-        path: "cz-conventional-changelog",
-    }
+  commitizen: {
+    path: "cz-conventional-changelog"
+  }
 };
 
 const jsExts = "*.{js,jsx}";
 const lintStaged = "lint-staged";
 if (useEslint) {
-    pkgjson[ lintStaged ] = {
-        ...pkgjson[ lintStaged ],
-        [ jsExts ]: [
-            useNpx
-                ? "npx eslint --fix"
-                : "eslint --fix",
-            "git add"
-        ]
-    }
+  pkgjson[lintStaged] = {
+    ...pkgjson[lintStaged],
+    [jsExts]: [useNpx ? "npx eslint --fix" : "eslint --fix", "git add"]
+  };
 }
 const tsExts = "*.{ts,tsx}";
 if (useTslint) {
-    pkgjson[ lintStaged ] = {
-        ...pkgjson[ lintStaged ],
-        [ tsExts ]: [
-            useNpx
-                ? "npx tslint --fix"
-                : "tslint --fix",
-            "git add"
-        ]
-    }
+  pkgjson[lintStaged] = {
+    ...pkgjson[lintStaged],
+    [tsExts]: [useNpx ? "npx tslint --fix" : "tslint --fix", "git add"]
+  };
 }
 
-fs.writeFileSync(
-    './package.json',
-    JSON.stringify(pkgjson, null, 4),
-);
+fs.writeFileSync("./package.json", JSON.stringify(pkgjson, null, 4));
